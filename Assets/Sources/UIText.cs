db7cbca9ed0125f6acf521454d85d488a0dc62d1
batoust <thebatoust@gmail.com>
2017-11-18 12:41:53 +0100

    UI Updated

---
+++ Assets/Sources/UIText.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Text;
+using System.Collections.Generic;
+
+[ExecuteInEditMode()]
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class UIText : MonoBehaviour {
+
+    [SerializeField]
+    [TextArea]
+    public string content;
+    [SerializeField]
+    private List<string> styles = new List<string>();
+
+    private TextMeshProUGUI text;
+
+    private void Reset()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        Apply();
+    }
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            Apply();
+        }
+    }
+#endif
+
+    private string Format(string content)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach(var style in styles)
+        {
+            sb.Append(string.Format("<style=\"{0}\">", style));
+        }
+        sb.Append(content);
+        foreach(var style in styles)
+        {
+            sb.Append("</style>");
+        }
+        
+        return sb.ToString();
+    }
+
+    public string GetStylizedText(string content)
+    {
+        return Format(content);
+    }
+
+    public void Apply()
+    {
+        text.text = Format(content);
+    }
+}
